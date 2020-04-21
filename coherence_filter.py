5c683ac78fb84785eec2b11893f53fec3f4f7b7e
Mylene Jacquemart <mjacqu@gmail.com>
2020-04-21 15:36:33 -0600

    Turned into function

--- coherence_filter.py
+++ coherence_filter.py
@@ -6,27 +6,21 @@ import re
 import datetime
 from itertools import compress
 
-coh_th = 0.55#threshold of mean coherence for filtering
 
-tiff_path = '/home/myja3483/Landslides/Bigsur/Results/coherence_TS'
-tiff_name = '*coherence.tif'
-
-tiff_list = glob.glob(os.path.join(tiff_path, tiff_name))
-
-poly_path = '/home/myja3483/isce_tools/MudCreek/'
-poly_name = 'ref_slope_large.geojson'
-
-dates = [re.search(r'[0-9]{8}_[0-9]{8}', l).group(0) for l in tiff_list]
-
-stats = [zonal_stats(os.path.join(poly_path,poly_name), l)[0] for l in tiff_list]
-
-filter = [s['mean'] < coh_th for s in stats]
-
-#generate list of dates to REMOVE
-remove_dates = list(compress(dates, filter))
-print('Coherence threshold: ' + str(coh_th))
-print('Writing ' + str(len(remove_dates)) + ' low coherence date pairs to file.')
-
-with open('low_coherence.txt', 'w') as f:
-    for item in remove_dates:
-        f.write("%s\n" % item)
+def filt_coh (c, tiff_path, tiff_name, poly_path, poly_name, write = False):
+    tiff_list = glob.glob(os.path.join(tiff_path, tiff_name))
+    dates = [re.search(r'[0-9]{8}_[0-9]{8}', l).group(0) for l in tiff_list]
+    stats = [zonal_stats(os.path.join(poly_path,poly_name), l)[0] for l in tiff_list]
+    filter = [s['mean'] < c for s in stats]
+    #generate list of dates to REMOVE
+    remove_dates = numpy.sort(list(compress(dates, filter)))
+    print('Coherence threshold: ' + str(c))
+    if write == True:
+        print('Writing ' + str(len(remove_dates)) + ' low coherence date pairs to file.')
+        with open('low_coherence'+ str(c)+'.txt', 'w') as f:
+            for item in remove_dates:
+                f.write("%s\n" % item)
+    else:
+        print(str(len(remove_dates)) + ' pairs below ' + str(c) + ' coherence threshold:')
+        print(remove_dates)
+    return remove_dates
